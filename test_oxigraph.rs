50d4cc89357a10bc92b59fbe699da03981036f98
W4R3Z <w4r3z@lex.local>
2026-03-30 06:29:42 -0700

    Clean up stray test files

--- test_oxigraph.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    let _ = oxigraph::sparql::QueryOptions::default().with_default_graph_as_union();
-}
--- test_union.rs
+++
@@ -1,5 +0,0 @@
-use oxigraph::sparql::Query;
-fn test() {
-    let mut q = Query::parse("SELECT * WHERE {?s ?p ?o}", None).unwrap();
-    q.dataset_mut().set_default_graph_as_union();
-}
